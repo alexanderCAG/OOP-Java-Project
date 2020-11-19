7aad4cefcaed2ad568bfc8c3316471d96fbda931
Geoffroy78 <73698713+Geoffroy78@users.noreply.github.com>
2020-11-19 11:04:10 +0100

    Base de donnee

--- src/oop/java/project/OOPJavaProject.java
+++ src/oop/java/project/OOPJavaProject.java
@@ -5,6 +5,7 @@
  */
 package oop.java.project;
 
+import java.sql.*;
 import oop.java.project.GUI.*;
 
 /**
@@ -16,11 +17,21 @@ public class OOPJavaProject {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
         // TODO code application logic here
         
-        new Login();
-        
+        //new Login();
+        Class.forName("com.mysql.jdbc.Driver");
+
+        // url de connexion "jdbc:mysql://localhost:3305/usernameECE"
+        String urlDatabase = "jdbc:mysql://localhost:3308/Company";
+       // String urlDatabase = "jdbc:mysql://localhost:3308/jps?characterEncoding=latin1";
+
+        //création d'une connexion JDBC à la base 
+        Connection conn = DriverManager.getConnection(urlDatabase, "root", "");
+
+        // création d'un ordre SQL (statement)
+        //Statement stmt = conn.createStatement();
     }
     
 }
