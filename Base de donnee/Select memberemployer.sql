d7b69402c87e1452839d8190c796081e3a73d6b4
Geoffroy78 <73698713+Geoffroy78@users.noreply.github.com>
2020-11-19 11:27:25 +0100

    Base de donnee

---
+++ Base de donnee/Select memberemployer.sql
@@ -0,0 +1,28 @@
+use `Company`;
+
+-- Affichage job + Mise a jour
+-- select distinct namejob from job;
+
+-- Affichage des nouveaux emplois (nom et prenom)
+-- select namemem from memberemployer;
+
+
+-- Ajouter des nouveaux jobs
+-- INSERT INTO `Company`.Agency ( nameag ) VALUES ('Agence DAG');
+
+-- Afficher les candidats(Quelles informations retourner?)
+-- select * from jobseeker;
+
+-- Supprimer des jobs
+-- delete from job where namejob='';
+
+-- Evaluer la popularite des emplois (sur le programme il faudra additionner les deux)
+-- select count(namemem) from memberemployer where job=' ';
+-- select count(namenew) from newemployer where job='';
+
+-- Verifier si un compte existe deja
+-- select *from memberemployer;
+-- select *from newemployer;
+-- select *from Jobseeker;
+
+-- employer 
