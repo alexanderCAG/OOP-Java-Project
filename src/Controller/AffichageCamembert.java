1297f5e322995afddebe95445092213770843d65
Geoffroy78 <73698713+Geoffroy78@users.noreply.github.com>
2020-12-08 15:33:41 +0100

    f

--- src/Controller/AffichageCamembert.java
+++ src/Controller/AffichageCamembert.java
@@ -63,7 +63,7 @@ public class AffichageCamembert implements ActionListener{
             Connection conn=Connexion1();
         try{
             Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("Select count(id) from demandeemploi where metier='" + listJob[i] + "';");    //Compte le nombre de personne par job
+            ResultSet rs = stmt.executeQuery("Select count(id) from demandeemploi where metier='" + listJob[i] + "';");    //Compte le nombre de demande d'emploi par job
             while(rs.next()){
             nombrepersonnejob[i]=rs.getInt(1);
         }
